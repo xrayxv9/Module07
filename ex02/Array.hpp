bb4e9e97cf56e4933aceb4e9938c4ff5be5a4aff
Clement Morel <xrayxv9@gmail.com>
2025-06-10 16:31:16 +0200

    finished

---
+++ ex02/Array.hpp
@@ -0,0 +1,96 @@
+#pragma once
+
+#include <cstdlib>
+#include <iostream>
+
+
+template <typename T>
+
+class Array
+{
+	public:
+		Array() { _array = NULL; _size = 0; }
+	
+		Array(unsigned int n)
+		{
+			_array = new T[n];
+
+			for (unsigned int  i = 0; i < n; i++)
+				_array[i] = 0;
+			_size = n;
+		}
+		~Array() { delete [] _array; }
+
+		T &operator[](int index)
+		{
+			unsigned int num = index;
+
+			if (_array && _size > 0)
+			{
+				if ( index < 0 )
+					throw IndexTooLowException();
+				if ( num >= _size )
+					throw IndexTooHighException();
+				else
+					return _array[index];
+			}
+			throw NoSizeGivenException();
+		}
+
+		Array &operator=( const Array &cpy )
+		{
+			if (this->_array)
+				delete [] _array;
+			if (this != &cpy)
+			{
+				this->_size = cpy.size();
+				this->_array = new T[cpy.size()];
+				for (unsigned int i = 0; i < _size; i++)
+					this->_array[i] = cpy._array[i];
+			}
+			return *this;
+		}
+
+		Array( const Array &cpy )
+		{
+			_array = NULL;
+			_size = 0;
+			*this = cpy;
+		}
+
+		class IndexTooLowException : public std::exception
+		{
+			public:
+				const char * what() const throw()
+				{
+					return ("ERROR : index too low. impossible.");
+				}
+		};
+
+		class IndexTooHighException : public std::exception
+		{
+			public:
+				const char * what() const throw()
+				{
+					return ("ERROR : index too high. impossible.");
+				}
+		};
+
+		class NoSizeGivenException : public std::exception
+		{
+			public:
+				const char * what() const throw()
+				{
+					return ("ERROR : no size was given during the initialisation. impossible.");
+				}
+		};
+
+		unsigned int size() const
+		{
+			return(_size);
+		}
+
+	private:
+		T *_array;
+		unsigned int _size;
+};
---
+++ ex02/main.cpp
@@ -0,0 +1,45 @@
+#include "Array.hpp"
+
+int main()
+{
+	Array<int>	tab(10);
+	int			*array = new int[10];
+	srand(time(0));
+	int value;
+
+	std::cout << std::endl << "BASE" << std::endl << std::endl;
+	for (int i = 0; i < 10; i++)
+		std::cout << "Array : " << tab[i] << std::endl;
+
+	std::cout << std::endl << "CHANGE VALUES" << std::endl << std::endl;
+	for (int i = 0; i < 10; i++)
+	{
+		value = rand() % 20;
+		array[i] = value;
+		tab[i] = value;
+	}
+
+	std::cout << std::endl << "PRINT CHANGED VALUES" << std::endl << std::endl;
+	for (int i = 0; i < 10; i++)
+	{
+		std::cout << "int : " << array[i] << std::endl;
+		std::cout << "Array : " << tab[i] << std::endl;
+	}
+
+	std::cout << std::endl << "COPY VALUES" << std::endl << std::endl;
+	Array<int> t(tab);
+
+	for (int i = 0; i < 10; i++)
+	{
+		std::cout << "cpy_tab : " << t[i] << std::endl;
+		std::cout << "Array : " << tab[i] << std::endl;
+	}
+
+	t[0] = rand() % 1999;
+
+	std::cout << std::endl << "MODIFIED FIRST VALUE" << std::endl << std::endl;
+	std::cout << "original : " << tab[0] << std::endl;
+	std::cout << "modified : " << t[0] << std::endl;
+
+	delete [] array;
+}
